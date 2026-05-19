7c2b5c252209f389aba41c830ef075755f56bae8
Antonio <antoniosalvaterracroissant@gmail.com>
2026-05-19 08:48:21 +0200

    Fix usage message in bootstrap.sh

--- code/bootstrap.sh
+++ code/bootstrap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[[ $# -ne 6 ]] && || { echo "Usage $0
+[[ $# -ne 6 ]] && || { echo "Usage $0 <num_receivers> <num_pickers> <num_packers> <queue_capacity> <num_suppliers> <inventory.csv>"}
